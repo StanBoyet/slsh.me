4711a3b5dd42a4ea3bdc755f0faa3377b91ff08f
Stanislas Boyet <stan.boyet@gmail.com>
2026-04-11 12:47:24 +0200

    Always show landing page, swap nav CTAs for authenticated users
    - Remove authenticated redirect from PagesController#landing
    - Show "Go to the app" button when logged in (nav + footer)
    - Show "Sign in" / "Get started" when logged out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,5 @@ class PagesController < ApplicationController
   allow_unauthenticated_access
 
   def landing
-    redirect_to app_url("/links"), allow_other_host: true if authenticated?
   end
 end
